4bf4a685ecddf89bf1e7b7c263012e3f8ebee18f
BABAHAJ Zakarya <z.babahaj@edu.umi.ac.ma>
2023-11-09 22:22:24 +0100

    create authentification middlwares

---
+++ auth.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('./db');
+const Gestionnaire = require('./db');
+const Etudiant = require('./db');
+
+const router = require("express").Router();
+const secretKey = 'KEY_TP_EXPRESS';
+
+
+const gestionnaireMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: 'Token is missing' });
+    }
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        if (decoded && decoded.type === 'gestionnaire') {
+            req.user = decoded;
+            next();
+        } else {
+            return res.status(403).json({ message: 'Forbidden: Access denied' });
+        }
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+const etudiantMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token is missing' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        if (decoded && decoded.type === 'etudiant') {
+            req.user = decoded;
+            next();
+        } else {
+            return res.status(403).json({ message: 'Forbidden: Access denied' });
+        }
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+
+router.post('/gestionnaire-login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await Gestionnaire.findOne({ username, password});
+        if (user) {
+            const token = jwt.sign({ id: user.username, type: 'gestionnaire' }, secretKey);
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid username or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+router.post('/etudiant-login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await Etudiant.findOne({ username, password});
+        if (user) {
+            const token = jwt.sign({ id: user.cne, type: 'etudiant'}, secretKey);
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid username or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+router.get('/gestionnaire-route', gestionnaireMiddleware, (req, res) => {
+    res.json({ message: 'Welcome, gestionnaire!' });
+});
+
+router.get('/etudiant-route', etudiantMiddleware, (req, res) => {
+    res.json({ message: 'Welcome, etudiant!' });
+});
+
+module.exports = router;
--- db.js
+++ db.js
@@ -5,6 +5,18 @@ mongoose.connect('mongodb://localhost/Edugate', {
   useUnifiedTopology: true,
 });
 
+const etudiantSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  cne: String,
+  fullname: String,
+});
+
+const gestionnaireSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
 const inscriptionSchema = new mongoose.Schema({
   idInscription: String,
   cne: String,
@@ -31,8 +43,10 @@ const dmModificationSchema = new mongoose.Schema({
   accepted: Boolean,
 });
 
+const Gestionnaire = mongoose.model('gestionnaire', gestionnaireSchema);
+const Etudiant = mongoose.model('etudiant', etudiantSchema);
 const Inscription = mongoose.model('inscription', inscriptionSchema);
 const Filiere = mongoose.model('filiere', filiereSchema);
 const DmModification = mongoose.model('dmModification', dmModificationSchema);
 
-module.exports = { Inscription, Filiere, DmModification, mongoose};
+module.exports = {Gestionnaire,Etudiant,Inscription, Filiere, DmModification, mongoose};
